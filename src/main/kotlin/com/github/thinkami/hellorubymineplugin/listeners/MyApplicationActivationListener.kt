8ddf23414db90da0bd2f861350da5dd51d67acff
GitHub Action <action@github.com>
2024-02-28 23:41:35 +0000

    Template cleanup

--- src/main/kotlin/com/github/thinkami/hellorubymineplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/thinkami/hellorubymineplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.thinkami.hellorubymineplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
